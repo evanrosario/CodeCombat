ab225027456ca92c36e038509d1338fb4a9a6bc7
evanrosario <49211112+evanrosario@users.noreply.github.com>
2020-01-26 11:50:57 -0500

    FenceBuilder.uc
    Real Answers

--- Mountain/FenceBuilder/FenceBuilder.py
+++ Mountain/FenceBuilder/FenceBuilder.py
@@ -9,15 +9,15 @@ x2 = customer.rightTop.x
 y2 = customer.rightTop.y
 step = 4
 
-# Let's build the bottom side.
-for x in range(x1 + step, x2 + 1, step):
-    hero.buildXY("fence", x, y1)
-# Then the right side.
-for y in range(y1 + step, y2 + 1, step):
-    hero.buildXY("fence", x2, y);
-# Build the top side.
-for x in range(x1 + step, x2 + 1, step):
-    hero.buildXY("fence", x, y2)
 # Build the left side.
 for y in range(y1, y2 + 1, step):
     hero.buildXY("fence", x1, y);
+# Build the top side.
+for x in range(x1 + step, x2 + 1, step):
+    hero.buildXY("fence", x, y2)
+# Then the right side.
+for y in range(y1 + step, y2 + 1, step):
+    hero.buildXY("fence", x2, y);
+# Let's build the bottom side.
+for x in range(x1 + step, x2 + 1, step):
+    hero.buildXY("fence", x, y1)
